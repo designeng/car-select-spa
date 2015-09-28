38bc0f131c82647895ccd5045c9b0a3d65fb3813
Denis Savenok <denissavenok@gmail.com>
2015-09-29 00:48:56 +0400

    tests inited and passed for plugins/marionette/components/tabs

--- client/coffee/plugins/marionette/components/tabs.coffee
+++ client/coffee/plugins/marionette/components/tabs.coffee
@@ -1,14 +1,26 @@
 define [
     'underscore'
+    'backbone'
     'marionette'
-], (_, Marionette) ->
+], (_, Backbone, Marionette) ->
+
+    class TabsItemView extends Marionette.ItemView
+        tagName: "li"
+        className: "tabs-item"
+
+    class TabsView extends Marionette.CollectionView
+        childView: TabsItemView
 
     return (options) ->
         createTabsFactory = (resolver, compDef, wire) ->
-            tabsView = new Marionette.CollectionView()
             wire(compDef.options).then (options) ->
-
-            resolver.resolve tabsView
+                items = _.map _.keys(options.labels), (label) ->
+                    label: label
+                    href: options.labels[label]
+                tabsView = new TabsView({
+                    collection: new Backbone.Collection(items)
+                })
+                resolver.resolve tabsView
 
         pluginInstance = 
             factories: 
--- client/js/plugins/marionette/components/tabs.js
+++ client/js/plugins/marionette/components/tabs.js
@@ -1,11 +1,52 @@
-define(['underscore', 'marionette'], function(_, Marionette) {
+var __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+define(['underscore', 'backbone', 'marionette'], function(_, Backbone, Marionette) {
+  var TabsItemView, TabsView, _ref, _ref1;
+  TabsItemView = (function(_super) {
+    __extends(TabsItemView, _super);
+
+    function TabsItemView() {
+      _ref = TabsItemView.__super__.constructor.apply(this, arguments);
+      return _ref;
+    }
+
+    TabsItemView.prototype.tagName = "li";
+
+    TabsItemView.prototype.className = "tabs-item";
+
+    return TabsItemView;
+
+  })(Marionette.ItemView);
+  TabsView = (function(_super) {
+    __extends(TabsView, _super);
+
+    function TabsView() {
+      _ref1 = TabsView.__super__.constructor.apply(this, arguments);
+      return _ref1;
+    }
+
+    TabsView.prototype.childView = TabsItemView;
+
+    return TabsView;
+
+  })(Marionette.CollectionView);
   return function(options) {
     var createTabsFactory, pluginInstance;
     createTabsFactory = function(resolver, compDef, wire) {
-      var tabsView;
-      tabsView = new Marionette.CollectionView();
-      wire(compDef.options).then(function(options) {});
-      return resolver.resolve(tabsView);
+      return wire(compDef.options).then(function(options) {
+        var items, tabsView;
+        items = _.map(_.keys(options.labels), function(label) {
+          return {
+            label: label,
+            href: options.labels[label]
+          };
+        });
+        tabsView = new TabsView({
+          collection: new Backbone.Collection(items)
+        });
+        return resolver.resolve(tabsView);
+      });
     };
     pluginInstance = {
       factories: {
